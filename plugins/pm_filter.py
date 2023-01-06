9f7759350bc4daef597b71b3698705b9df1d7cba
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-01-06 20:00:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -683,7 +683,12 @@ async def auto_filter(client, msg, spoll=False):
     btn.insert(0,
         [
             InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='http://t.me/How_to_download_bot'),
-            InlineKeyboardButton(text=" 😒 Click here to Check spelling 👍", url='https://www.google.com/{search}')
+            
+        ],
+               
+        [
+            
+            InlineKeyboardButton(text=" 😒 Click here to Check spelling 👍", url='https://www.google.com/searcg?q=query')
         ]
     )
 
